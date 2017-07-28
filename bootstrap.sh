8a0a8fc2feb21825875933bb6b227ef4fefa84f7
dinghao2020 <18001326539@163.com>
2017-07-28 09:55:39 +0800

    passwd

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
 
 chown webvirtmgr /var/run/libvirt/libvirt-sock
 /usr/bin/python /webvirtmgr/manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'Admin1234')" | /usr/bin/python /webvirtmgr/manage.py shell
 
 fi
 
